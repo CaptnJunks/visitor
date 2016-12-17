2e0f2cccd93754e5dba1b7d412ce363f4d0290f3
James Zeigler <jams.zeigler@gmail.com>
2016-12-16 16:32:41 -0800

    redeploy

--- build/generated-sources/ap-source-output/mainBean/Entities/Visitor_.java
+++ build/generated-sources/ap-source-output/mainBean/Entities/Visitor_.java
@@ -5,11 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-<<<<<<< HEAD
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-12-16T15:54:40")
-=======
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-12-08T23:58:00")
->>>>>>> 1e881b3a4042bdb019012c65cbec7595643d853e
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-12-16T16:32:13")
 @StaticMetamodel(Visitor.class)
 public class Visitor_ { 
 
